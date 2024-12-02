deaf2d69cb723a96b2b194f7bb086aa298486fe3
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-12-02 10:53:33 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'About', component: AboutMeComponent},
+    {path: 'Project', component: ProjectsComponent},
+
+];
